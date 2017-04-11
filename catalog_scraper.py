0f779378a9c1ff3486d291af67557116d168c8a2
Jeffrey Lee <jel347@ucsd.edu>
2017-04-11 02:34:19 -0700

    Successfully refactored and returned dfs on courses

---
+++ catalog_scraper.py
@@ -0,0 +1,96 @@
+import requests
+import csv
+import time
+import re
+from bs4 import BeautifulSoup
+
+CSE_CATALOG_URL = 'https://cse.ucsd.edu/undergraduate/courses/prerequisites-cse-undergraduate-classes'
+
+def clean(string, utf8=None):
+
+	string = string.encode('utf-8')
+
+	replace_values = [(u"\xa0".encode('utf-8'), " "), (u"\u2014".encode('utf-8'), "-"), \
+					(u"\u0027".encode('utf-8'), "'"), (u"\u2013".encode('utf-8'), "-"), \
+					(u"\u2019".encode('utf-8'), "'")]
+
+	for utf, new in replace_values:
+		string = string.replace(utf, new)
+
+	if utf8 and utf8 is True:
+		return " ".join(string.split()).encode('utf-8')
+
+	return " ".join(string.split())
+
+def extract_catalog_dict(record):
+
+	course_ids = []
+	course_titles = []
+	course_prereqs = []
+
+	if record:
+		start = time.time()
+		print "Requesting access to the UCSD catalog page at %s..." % CSE_CATALOG_URL
+	page = requests.get(CSE_CATALOG_URL)
+
+	if record:
+		print "Webscraping the catalog..."
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	table = soup.find('table')
+
+	# Find all the <tr> tag pairs, skip the first one, then for each.
+	for row in table.find_all('tr')[1:]:
+
+		col = row.find_all('td')
+
+		cse_course = clean(col[0].text.strip(), True)
+		course_ids.append(cse_course)
+
+		title = clean(col[1].text.strip(), True)
+		course_titles.append(title)
+
+		#creates a list of preqreqs, with a few rules
+		#NO CO-REQUISITES
+		#NO SELF-DEPENDENCIES
+		prereqs = col[2].text.strip().upper().split('CO-REQUISITE', 1)[0]
+		prereqs = clean(prereqs.replace(cse_course, ""))
+
+		# 1 capital letter, 0+ letters, space, 1+ digits, 0 or 1 letter
+		# i.e. 'CSE 30' or 'CSE 8A'
+		pattern = "[A-Z][a-zA-Z]*\s?[0-9]+[a-zA-Z]?"
+
+		# creates a list of prereqs based on the regex
+		filtered_prereqs = re.findall(pattern, prereqs)
+
+		course_prereqs.append(filtered_prereqs)
+
+	if record:
+		end = time.time()
+		print "Completed scraping... %.3f seconds" % (end-start)
+	return course_ids, course_titles, course_prereqs
+
+def write_catalog(ids, titles, prereqs, record):
+
+	if record:
+		start = time.time()
+		print "Writing to the csv file 'courses.csv'..."
+	
+	for i in range(len(prereqs)):
+		prereqs[i] = " ".join(prereqs[i])
+
+	with open('courses.csv', 'wb') as csvfile:
+
+		writer = csv.writer(csvfile)
+
+		rows = zip(ids, titles, prereqs)
+
+		writer.writerows(rows)
+
+	if record:
+		end = time.time()
+		print "Completed writing to file... %.3f seconds" % (end-start)
+
+#ids, titles, pres = extract_catalog_dict(record=True)
+#write_catalog(ids, titles, pres, record=True)
+#print "Done."
---
+++ dag_analyzer.py
@@ -0,0 +1,106 @@
+from catalog_scraper import extract_catalog_dict
+
+courses = {}
+
+ids, titles, pres = extract_catalog_dict(record=False)
+
+catalog_info = zip(ids, titles, pres)
+
+def insert_into_course(key, value):
+
+	if not type(value) is dict:
+		raise TypeError("Value is not a dictionary!")
+
+	if key not in courses:
+		courses[key] = value
+	else:
+		courses[key].update(value)
+
+def dfs(course_info):
+	"""
+		dfs(course_info)
+			course_info -> 3-tuple in the format (string, string, list of strings)
+
+		updates the courses dictionary to include the specified course and its prereqs
+	"""
+
+	#gets the id of the course
+	i = course_info[0]
+
+	#check if the courses dict already has the course path defined
+	if i in courses:
+		return courses[i]
+
+	#retrieve the list of preqres for the course
+	pr = [p for p in course_info[2]]
+
+	# memoization table, check if keys in dict already exists
+	for p in course_info[2]:
+
+		if p in courses:
+
+			insert_into_course(i, {p: courses[p]})
+			pr.remove(p)
+
+	# check if there are no prereqs (empty list)
+	if not course_info[2]:
+		courses[i] = i
+		return {i : i}
+
+	course_stack = [pr]
+
+	#stack or dfs to reproduce a DAG
+	while(course_stack):
+
+		curr_p = course_stack.pop()
+
+		#iterate through the id of each prereq in the list
+		for curr_i in curr_p:
+	
+			#check if the id is in the courses/ids
+			if curr_i not in courses and curr_i in ids:
+				
+				#retrieve the course_info from the catalog with the matching id
+				c_info = next(x for x in catalog_info if x[0] == curr_i)
+
+				#retrieve prereq dictionary of current prereq 
+				prereqs = dfs(c_info)
+
+				#merge the prereq dictionary to the courses's current prereq dict
+				insert_into_course(i, prereqs)
+
+			#memoization in action
+			elif curr_i in courses:
+
+				insert_into_course(i, {curr_i: courses[curr_i]})
+
+			#edge case, courses catalog doesn't contain the prereq
+			elif curr_i not in ids:
+
+				insert_into_course(i, {curr_i : curr_i})
+
+	return {i: courses[i]}
+
+def determine_path(course):
+
+	course_path = dfs(course)
+	print course_path
+	print "Root:", course[0]
+
+	print_path(course_path[course[0]], 2)
+
+def print_path(dict_path, spaces):
+
+	if type(dict_path) is str:
+		return
+
+	else:
+		for key in dict_path.iterkeys():
+			print " "*spaces + "Child: " + key
+			print_path(dict_path[key], spaces+2)
+
+#for course in catalog_info:
+#	dfs(course)
+
+#print dfs(catalog_info[11])
+determine_path(catalog_info[11])
